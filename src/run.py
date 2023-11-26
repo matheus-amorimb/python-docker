330b3ffb80d5f684d008cb9343b785693334f0ee
Matheus Batista <mbatista.sarti@gmail.com>
2023-11-26 13:45:33 -0300

    configurando pre-commit

--- src/run.py
+++ src/run.py
@@ -1,10 +1,11 @@
-from flask import Flask
 import os
+import os.path
 
-
-
+from flask import Flask
 
 app = Flask(__name__)
+
+
 @app.route('/', methods=['GET'])
 def hello_world():
     return 'Olá, está tudo funcionando por aqui'
